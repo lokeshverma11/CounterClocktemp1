b8652ad2785a2d4ec1c88fb8808e9e90509dd17c
lokeshverma11 <lokeshverma111.lv@gmail.com>
2022-11-05 16:22:22 +0530

    final

--- myJS.js
+++ myJS.js
@@ -1,4 +1,5 @@
-const date = new Date();
+const time = new Date();
+var date = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 let clicks=1;
 
 document.getElementById("currentTime").innerHTML = date;
@@ -8,8 +9,10 @@ function countClicks()
 {
     clicks = clicks + 1;
     document.getElementById("counter").innerHTML = clicks;
-    if(clicks%2===0)
+    if(clicks%2===0){
         document.getElementById("currentTime").style.color = "green";
-    else
+        document.getElementById('a').style.backgroundImage="url(sun1.jpg)";}
+    else{
         document.getElementById("currentTime").style.color = "red";
+        document.getElementById('a').style.backgroundImage="url(sun2.jpg)";}
 }
